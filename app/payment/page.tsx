140131508d7e0680729bf62c2641f0c767362f96
tanzir ibne ali <tanziribneali@gmail.com>
2024-10-08 15:02:00 +0600

    delete false-test & checkout-form file & remove everything from payment page

--- app/payment/page.tsx
+++ app/payment/page.tsx
@@ -1,23 +1,6 @@
-"use client"
-import CheckoutForm from '@/components/shared/checkout-form';
-import { Elements } from '@stripe/react-stripe-js';
-import { loadStripe } from '@stripe/stripe-js';
-
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_PUBLISHABLE_KEY || '');
 
 const Payment = () => {
-    const options = {
-        mode: "payment",
-        currency: "usd",
-        amount: 50
-      }
-    return (
-        <div className='w-[300px] mx-auto my-14 '>
-        <Elements stripe={stripePromise} options={options}>
-      <CheckoutForm />
-    </Elements>
-        </div>
-    );
+
 };
 
 export default Payment;
